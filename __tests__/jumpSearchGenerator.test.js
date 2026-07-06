e6d1fc1ffe68f7ded8f67263ecefe9ffa8a0d195
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-06 15:18:59 +0530

    test: add unit tests for features/algorithms/array/jumpSearchLogic.js (#2881)

---
+++ __tests__/jumpSearchGenerator.test.js
@@ -0,0 +1,6 @@
+const module = require("features/algorithms/array/jumpSearchLogic.js");
+describe("jumpSearchGenerator", () => {
+  test("exports expected API", () => {
+    expect(module).toBeDefined();
+  });
+});
